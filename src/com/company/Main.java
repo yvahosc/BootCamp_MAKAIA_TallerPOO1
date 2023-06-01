0c0ab04c5a48eb98c782d52e8979a7c8a2929ef5
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-06-01 10:19:26 -0500

    Implementación de la clase Carro en el método Main.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    //Implementación clase Carro
+
+        //Creación primer objeto e implementación de sus métodos.
+        Carro carro1 = new Carro("Chevrolet", "Onix", 2023,
+                "Blanco");
+
+        System.out.println("\nCarro 1:");
+        carro1.acelerar();
+        carro1.frenar();
+        carro1.girar("derecha");
+        carro1.mostrarInformacion();
+
+        //Creación segundo objeto e implementación de sus métodos.
+        Carro carro2 = new Carro("Ford", "Fiesta", 2023,
+                "Negro");
+
+        System.out.println("\nCarro 2:");
+        carro2.acelerar();
+        carro2.frenar();
+        carro2.girar("izquierda");
+        carro2.mostrarInformacion();
     }
 }
