f8d7e411b03a03d180f1ff7bda0aecf5af11396c
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-06-01 11:56:02 -0500

    Creación de la estructura de la clase EquipoFutbol con sus atributos, constructor, getters, setters y métodos adicionales.

---
+++ src/com/company/EquipoFutbol.java
@@ -0,0 +1,58 @@
+package com.company;
+
+
+import java.util.ArrayList;
+
+public class EquipoFutbol {
+    private String nombre;
+    private String ciudad;
+    private ArrayList<Jugador> jugadores;
+
+    public EquipoFutbol(String nombre, String ciudad, ArrayList<Jugador> jugadores) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.jugadores = jugadores;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public ArrayList<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(ArrayList<Jugador> jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    public void agregarJugador(Jugador jugador){
+        jugadores.add(jugador);
+    }
+
+    public void mostrarInformacion(){
+        System.out.println("Objeto de la clase EquipoFutbol, con nombre: " +
+                getNombre() + "; ciudad: " + getCiudad() + "; y jugadores: ");
+        for (int i = 0; i < jugadores.size(); i++) {
+            if(jugadores.size() == i - 1){
+                System.out.print(getJugadores().get(i).getNombre() + " " +
+                        getJugadores().get(i).getApellido() + ".\n");
+            }else{
+                System.out.print(getJugadores().get(i).getNombre() + " " +
+                        getJugadores().get(i).getApellido() + ", ");
+            }
+        }
+    }
+}
