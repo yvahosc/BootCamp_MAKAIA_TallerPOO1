60619bb97f7351bb7021fb4cdc7d90da8def91f4
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-06-01 12:19:40 -0500

    Implementación de las clases Jugador y EquipoFutbol en el método Main.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -24,5 +26,33 @@ public class Main {
         carro2.frenar();
         carro2.girar("izquierda");
         carro2.mostrarInformacion();
+
+        //Implementación clase EquipoFutbol
+
+        //Creación de objetos de la clase Jugador y lista de jugadores.
+        Jugador jugador1 = new Jugador("Jaime", "Vardy");
+        Jugador jugador2 = new Jugador("James", "Maddison");
+        ArrayList<Jugador> jugadores = new ArrayList<>();
+        jugadores.add(jugador1);
+        jugadores.add(jugador2);
+
+        //Creación objeto de la clase EquipoFutbol e implementación de sus
+        // métodos.
+        EquipoFutbol equipo1 = new EquipoFutbol("Leicester City FC",
+                "Leicester", jugadores);
+
+        System.out.println("\nEquipo 1:");
+        equipo1.mostrarInformacion();
+
+        //Creación de otros objetos de la clase Jugadores para agregarlos al
+        // equipo.
+        Jugador jugador3 = new Jugador("Kelechi", "Iheanacho");
+        Jugador jugador4 = new Jugador("Marc", "Albrighton");
+        equipo1.agregarJugador(jugador3);
+        equipo1.agregarJugador(jugador4);
+
+
+        System.out.println("\nEquipo 1 después de agregar más jugadores:");
+        equipo1.mostrarInformacion();
     }
 }
